7aa861fe97e41498a781c9bd2015e2fb9c582d26
jarintasnimmim <155748404+jarintasnimmim@users.noreply.github.com>
2025-03-11 11:01:47 +0600

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,49 @@
+#Print your name.
+print('Jarin Tasnim Mim')
+
+#Input your batch number using input() function.
+batch = input('Enter your batch number:')
+print(batch)
+
+#Create 5 different valid Variables and print.
+Ramadan = 8
+print(Ramadan)
+
+_item = 15
+print(_item)
+
+Dessert_item = 7
+print(Dessert_item)
+
+std_213 = 213002111
+print(std_213)
+
+FAVOURITE_SNAKE = ('Cookies')
+print(FAVOURITE_SNAKE)
+
+#Create Pascal Case Variable.
+RamadanSpecialItem = 16
+print(RamadanSpecialItem)
+
+#Assign a single value to Multiple variables.
+apple = orange = watermelon = 20
+print(apple)
+print(orange)
+print(watermelon)
+
+#Input float value using input().
+x = float(input('Enter 1st Number: '))
+y = float(input('Enter 2nd Number: '))
+sum = x + y
+print(sum)
+
+#a = ‘Welcome to Django for web and Ai’ convert into upper case.
+a = 'Welcome to Django for web and Ai'
+print(a.upper())
+
+# b = ‘   Python’remove whitespace.
+b = ' Python'
+print(b.strip())
+
+
+
