435187b6b60b9a3c88d027a0ba1f939ec93bf91e
jarintasnimmim <155748404+jarintasnimmim@users.noreply.github.com>
2025-03-11 16:08:05 +0600

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,86 @@
+#print your friends name
+print('Fatema Akter')
+print('Maria Islam Mim')
+print('Tanzina Tanjin')
+print('Shabiha Sultana')
+
+#print name
+print('Jarin Tasnim Mim')
+
+#Input your batch number or any thing using input() function.
+batch_number = input('Enter your batch number:')
+print(batch_number)
+
+name = input('Enter your name:')
+print(name)
+id = input('Enter your Std id:')
+print(id)
+cse_course = input('Enter your course name:')
+print(cse_course)
+
+#Create Pascal Case Variable.
+SnacksItemVariable = 'Chicken Pakoda'
+print(SnacksItemVariable)
+
+#Create Camel Case Variable
+dessertItemVariable = 'Fruit custard'
+print(dessertItemVariable)
+
+#Create Snake Case Variable
+summer_fruits_name = 'Watermelon'
+print(summer_fruits_name)
+
+#Assign a single value to Multiple variables.
+
+x = y = z = 30
+print(x)
+print(y)
+print(z)
+
+#Assign multiple value to multiple variables
+a,b,c = 20,15,30
+print(a)
+print(b)
+print(c)
+
+#Input float value using input().
+x = float(input('Enter 1st number:'))
+y = float(input('Enter 2nd number:'))
+sub = x - y
+sum = x + y
+print(sub)
+print(sum)
+
+#a = ‘Welcome to Studymart’ convert into upper case.
+a = 'Welcome to Studymart'
+print(a.upper())
+print(a.lower())
+
+#Slicing
+m = 'Jarin Tasnim Mim'
+print(m[0:7])    #slice From the start
+print(m[7:])     #slice To the end
+print(m[7:12])   #Range
+
+#Remove Whitespace
+b = '    Green University Of Bangladesh'
+print(b.strip())
+c = '   Department of CSE'
+print(c.strip())
+
+#Replace
+s = 'Welcome to python with Django'
+print(s.replace('o',"u"))
+
+p = 'Python API Development with Django'
+print(p.replace('t','i'))
+print(p.split())        #Split String:
+
+q = 'AI'
+print(p + q)   #String Concatenation
+
+#Count() method
+d = 'Welcome to GUB'
+print(d.count('o'))
+print(len(d))  #len()
+
